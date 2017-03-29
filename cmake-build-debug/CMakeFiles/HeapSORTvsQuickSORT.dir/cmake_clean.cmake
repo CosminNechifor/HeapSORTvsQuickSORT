9ea9d4ffaea93a4bb0d21013c94ad285a30ec068
Cosmin Petru <cosmin.nechifor96@gmail.com>
2017-03-29 17:49:54 +0300

    Project Started

---
+++ cmake-build-debug/CMakeFiles/HeapSORTvsQuickSORT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeapSORTvsQuickSORT.dir/main.cpp.obj"
+  "HeapSORTvsQuickSORT.pdb"
+  "HeapSORTvsQuickSORT.exe"
+  "HeapSORTvsQuickSORT.exe.manifest"
+  "libHeapSORTvsQuickSORT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HeapSORTvsQuickSORT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HeapSORTvsQuickSORT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HeapSORTvsQuickSORT.
+# This may be replaced when dependencies are built.
