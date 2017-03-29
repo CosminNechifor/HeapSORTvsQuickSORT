362112abfd2d7a7f44211e8abdee6bb5f2eff789
Cosmin Petru <cosmin.nechifor96@gmail.com>
2017-03-29 18:13:48 +0300

    Heapsort

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <cstdlib>
 #include <cstdio>
 
+using namespace std;
 #define MAX 20
+#define TRUE 1
+
 void quickSort(int vector[], int left, int right){
     int temp, min, max, mid;
     mid = vector[left + (right - left)/2];
@@ -23,21 +26,78 @@ void quickSort(int vector[], int left, int right){
 }
 
 
+int max (int *array, int size, int i, int left, int right) {
+    int index = i;
+    if (left < size && array[left] > array[index]) {
+        index = left;
+    }
+    if (right < size && array[right] > array[index]) {
+        index = right;
+    }
+    return index;
+}
+
+void heapify(int *array, int size, int i) {
+    while (TRUE) {
+        int maxIndex = max(array, size, i, 2 * i + 1, 2 * i + 2);
+        if (maxIndex == i) {
+            break;
+        }
+        int temp = array[i];
+        array[i] = array[maxIndex];
+        array[maxIndex] = temp;
+        i = maxIndex;
+    }
+}
+
+
+void heapSort(int *array, int size) {
+    int i;
+    for (i = (size - 2) / 2; i >= 0; i--) {
+        heapify(array, size, i);
+    }
+    for (i = 0; i < size; i++) {
+        ///Swap
+        int temp = array[size - i - 1];
+        array[size - i - 1] = array[0];
+        array[0] = temp;
+        ///recall
+        heapify(array, size - i - 1, 0);
+    }
+}
+
+
+
 int main() {
-    int vector[MAX];
+    int firstArray[MAX];
+    int secondArray[MAX];
     for (int i = 0; i < MAX ; ++i) {
-        vector[i] = rand()%(1000) + 1;
+        firstArray[i] = rand()%(1000) + 1;
+        secondArray[i] = firstArray[i];
     }
-    printf("\n\nVector inainte de QuickSort:");
+    cout << "\nInitial array:";
     for (int k = 0; k < 20; ++k) {
-        printf("%d ", vector[k]);
+        cout << firstArray[k] << " ";
     }
 
-    quickSort(vector, 0, MAX-1);
+    quickSort(firstArray, 0, MAX-1);
 
-    printf("\n\nVector dupa QUICKSORT:");
-    for (int k = 0; k < 20; ++k) {
-        printf("%d ", vector[k]);
+    cout << "\n\nSorted with quicksort!\n";
+    for (int k = 0; k < MAX; ++k) {
+        cout << firstArray[k] << " ";
     }
+
+    for (int i = 0; i < MAX; ++i) {
+        firstArray[i] = secondArray[i];
+    }
+
+
+    heapSort(firstArray, MAX);
+    cout << "\n\nSorted with heapsort!\n";
+    for (int i = 0; i < MAX; ++i) {
+        cout << firstArray[i] << " ";
+    }
+
+
     return 0;
 }
