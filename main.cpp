8cc570e1bcfb83d1f8f9bba97b195c24229357eb
Cosmin Petru <cosmin.nechifor96@gmail.com>
2017-03-30 17:43:13 +0300

    Alpha

--- main.cpp
+++ main.cpp
@@ -14,16 +14,27 @@ FILE *fileQuickSort = fopen("../QuickSort.txt", "w");
 void quickSort(int array[], int left, int right){
     int temp, min, max, mid;
     mid = array[left + (right - left)/2];
+    assignmentsQuickSort++;
     min = left;
     max = right;
     do{
-        while (array[min] < mid) min++;
-        while (array[max] > mid) max--;
+        while (array[min] < mid) {
+            min++;
+            comparisonQuickSort++;
+        }
+
+        while (array[max] > mid) {
+            max--;
+            comparisonQuickSort++;
+        }
+
 
         if (min <= max){
             temp = array[min];
             array[min++] = array[max];
             array[max--] = temp;
+            assignmentsQuickSort+=3;
+
         }
     }while(min < max);
     if (left < max) quickSort(array, left, max);
@@ -34,9 +45,11 @@ void quickSort(int array[], int left, int right){
 int max(int *array, int size, int i, int left, int right) {
     int index = i;
     if (left < size && array[left] > array[index]) {
+        comparisonHeapSort++;
         index = left;
     }
     if (right < size && array[right] > array[index]) {
+        comparisonHeapSort++;
         index = right;
     }
     return index;
@@ -51,6 +64,7 @@ void heapify(int *array, int size, int i) {
         int temp = array[i];
         array[i] = array[maxIndex];
         array[maxIndex] = temp;
+        assignmentsHeapSort += 3;
         i = maxIndex;
     }
 }
@@ -66,6 +80,7 @@ void heapSort(int *array, int size) {
         int temp = array[size - i - 1];
         array[size - i - 1] = array[0];
         array[0] = temp;
+        assignmentsHeapSort += 3;
         ///recall
         heapify(array, size - i - 1, 0);
     }
@@ -128,6 +143,7 @@ void generateBest_QuickSort(int arraySize){
 int partition_worst(int *array, int low, int high)
 {
     int pivot = array[high];    // pivot
+    assignmentsQuickSort++;
     int i = (low - 1);  // Index of smaller element
 
     for (int j = low; j <= high- 1; j++)
@@ -136,15 +152,18 @@ int partition_worst(int *array, int low, int high)
         // equal to pivot
         if (array[j] <= pivot)
         {
+            comparisonQuickSort++;
             i++;    // increment index of smaller element
             int temp = array[i];
             array[i] = array[j];
             array[j] = temp;
+            assignmentsQuickSort += 3;
         }
     }
     int temp = array[i+1];
     array[i+1] = array[high];
     array[high] = temp;
+    assignmentsQuickSort += 3;
     return (i + 1);
 }
 
@@ -188,8 +207,50 @@ void generateWorst_QuickSort(int arraySize){
     quickSort_worst(array, 0, arraySize-1);
 
     int sum = comparisonQuickSort + assignmentsQuickSort;
-
     fprintf(fileQuickSort, "%d,%d,%d\n", comparisonQuickSort, assignmentsQuickSort, sum);
+}
+
+
+
+///partition used at quick select
+int partition(int* array, int left, int right)
+{
+    int pivot = array[right];
+
+    while ( left < right )
+    {
+        while ( array[left] < pivot )
+            left++;
+
+        while ( array[right] > pivot )
+            right--;
+
+        if ( array[left] == array[right] )
+            left++;
+        else if ( left < right ) {
+            int tmp = array[left];
+            array[left] = array[right];
+            array[right] = tmp;
+        }
+    }
+    return right;
+}
+
+///quick select
+int quick_select(int* array, int left, int right, int k)
+{
+    if ( left == right )
+        return array[left];
+
+    int j = partition(array, left, right);
+    int length = j - left + 1;
+
+    if ( length == k )
+        return array[j];
+    else if ( k < length )
+        return quick_select(array, left, j - 1, k);
+    else
+        return quick_select(array, j + 1, right, k - length);
 }
 
 
@@ -197,21 +258,24 @@ int main() {
     fprintf(fileQuickSort, "\nQuickSort Avg case\n");
     fprintf(fileHeapSort, "\nHeapSort Avg case\n");
     for (int i = 100; i < MAX ; i+=100) {
-        printf("\n%d", i);
+        //printf("\n%d", i);
         generateAvgCase(i);
     }
 
     fprintf(fileQuickSort, "\nQuickSort Best case\n");
     for (int i = 100; i < MAX; i+=100) {
-        printf("\n%d", i);
+        //printf("\n%d", i);
         generateBest_QuickSort(i);
     }
 
     fprintf(fileQuickSort, "\n\nQuickSort Worst case\n");
     for (int i = 0; i < MAX; i += 100) {
-        printf("\n%d", i);
+        //printf("\n%d", i);
         generateWorst_QuickSort(i);
     }
+
+//    int array[] = {12, 11, 5, 3, 20, 1};
+//    printf("Second elemnt is %d.", quick_select(array, 0, 5, 2));
 
     return 0;
 }
